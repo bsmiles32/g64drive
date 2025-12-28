e13619cbf6776b19dada9d99818eceb22de18bba
Bobby Smiles <bobby.smiles32@gmail.com>
2025-12-28 23:41:15 +0100

    wip PI paginated read

--- ultrasave/pi/pi.go
+++ ultrasave/pi/pi.go
@@ -1,6 +1,7 @@
 package pi
 
 import (
+	"context"
 	"errors"
 )
 
@@ -8,6 +9,7 @@ var (
 	// Implementors of ParallelInterface may return
 	// this error if the requested operation is not supported.
 	ErrUnsupported = errors.New("operation is not supported")
+	ErrUnalignedBurst = errors.New("unaligned burst")
 )
 
 // Parallel Interface (PI) has a 32bit address space.
@@ -19,4 +21,88 @@ type Controller interface {
 	Write32(address Address, data uint32) error
 	ReadBurst(address Address, data []byte) error
 	WriteBurst(address Address, data []byte) error
+}
+
+func alignDown(address Address, bits int) Address {
+	mask := Address((1 << bits) - 1)
+	return (address &^ mask)
+}
+
+func alignUp(address Address, bits int) Address {
+	mask := Address((1 << bits) - 1)
+	return (address | mask) + 1
+}
+
+type BurstFn func(Address, []byte) error
+
+// Splits a DMA Read operation into suitable bursts such that:
+// * all burst have a size which is a multiple of 4 (to accomodate ultrasave constrains)
+// * all burst are 4-byte aligned, this is a bit conservative as PI only need 2-byte alignment (for specified behavior) but this eases the implementation.
+// * no burst will cross device page boundary (eg. 2^pageBits)
+// * only the minimal number of burst shall be emitted (eg. we always try to read up to the next limit)
+// * transparently handle unaligned transfers
+func Read(ctx context.Context, p []byte, address Address, pageBits int, readBurst BurstFn) error {
+	// Early return for empty reads
+	if len(p) == 0 {
+		return nil
+	}
+
+	pageSize := (1 << pageBits)
+	var page = make([]byte, pageSize)
+
+	// First burst realign transfer to next page, if reached, otherwise truncate to len(p).
+	// Due to ultrasave limitation we also need to ensure end-begin is a multiple of 4
+	// In case this require over-reading, we need to use a temporary buffer
+	// and copy back only what's needed into p.
+	begin := alignDown(address, 2)
+	end := alignDown(address+Address(len(p))+3, 2)
+	skip := int(address - begin)
+
+	burstEnd := alignUp(begin, pageBits)
+	if burstEnd > end {
+		burstEnd = end
+	}
+
+	burstSize := int(burstEnd - begin)
+	if err := readBurst(begin, page[:burstSize]); err != nil {
+		return err
+	}
+
+	idx := burstSize - skip
+	if len(p) < idx {
+		idx = len(p)
+	}
+	copy(p[:idx], page[skip:skip+idx])
+
+	if idx == len(p) {
+		return nil
+	}
+
+	// Next transfers should all be properly aligned without needing any over-reading
+	begin = burstEnd
+	for begin+Address(pageSize) < end {
+		if err := ctx.Err(); err != nil {
+			return err
+		}
+
+		// burstEnd < end by construction, no need to further limit burstEnd
+		burstEnd := alignUp(begin, pageBits)
+		burstSize := int(burstEnd - begin)
+		if err := readBurst(begin, p[idx:idx+burstSize]); err != nil {
+			return err
+		}
+
+		begin += Address(burstSize)
+		idx += burstSize
+	}
+
+	// Last transfer may need to over-read and copy back only required bytes.
+	skip = int(end - (address + Address(len(p))))
+	burstSize = int(end - begin)
+	if err := readBurst(begin, page[:burstSize]); err != nil {
+		return err
+	}
+	copy(p[idx:], page[:burstSize-skip])
+
+	return nil
 }
---
+++ ultrasave/pi/pi_test.go
@@ -0,0 +1,167 @@
+package pi
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func toChar(b byte) byte {
+	if b < 32 || b > 126 {
+		return '.'
+	}
+	return b
+}
+
+// returns an output similar to hexdump -C
+// It differs from encoding/hex.Dump mainly it 2 aspects (apart from being a naive implementation):
+// * will pad data to a multiple of 16 bytes
+// * will squeeze consecutive equal lines and print a * instead (very useful for sparse data like flash)
+func hexDump(data []byte) string {
+	// Pad data to a multiple of 16 to ease dumping logic
+	for len(data)%16 != 0 {
+		data = append(data, byte(0))
+	}
+
+	var b strings.Builder
+
+	printStar := true
+	for k := 0; k < len(data); k += 16 {
+		if k >= 16 && bytes.Equal(data[k:k+16], data[k-16:k]) {
+			if printStar {
+				fmt.Fprintf(&b, "*\n")
+				printStar = false
+			}
+			continue
+		}
+
+		printStar = true
+		fmt.Fprintf(&b, "%08x  %02x %02x %02x %02x %02x %02x %02x %02x  %02x %02x %02x %02x %02x %02x %02x %02x  |%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s|\n", k,
+			data[k+0], data[k+1], data[k+2], data[k+3], data[k+4], data[k+5], data[k+6], data[k+7],
+			data[k+8], data[k+9], data[k+10], data[k+11], data[k+12], data[k+13], data[k+14], data[k+15],
+			string(toChar(data[k+0])), string(toChar(data[k+1])), string(toChar(data[k+2])), string(toChar(data[k+3])),
+			string(toChar(data[k+4])), string(toChar(data[k+5])), string(toChar(data[k+6])), string(toChar(data[k+7])),
+			string(toChar(data[k+8])), string(toChar(data[k+9])), string(toChar(data[k+10])), string(toChar(data[k+11])),
+			string(toChar(data[k+12])), string(toChar(data[k+13])), string(toChar(data[k+14])), string(toChar(data[k+15])),
+		)
+	}
+
+	return b.String()
+}
+
+func TestRead(t *testing.T) {
+
+	type ReadBurstInfo struct {
+		address Address
+		size    int
+	}
+
+	testCases := []struct {
+		address       Address
+		size          int
+		pageBits      int
+		expectedReads []ReadBurstInfo
+	}{
+		{address: 1, size: 0, pageBits: 9, expectedReads: []ReadBurstInfo{}},
+		{address: 0, size: 1, pageBits: 9, expectedReads: []ReadBurstInfo{
+			{address: 0, size: 4},
+		},
+		},
+		{address: 1, size: 1, pageBits: 9, expectedReads: []ReadBurstInfo{
+			{address: 0, size: 4},
+		},
+		},
+		{address: 2, size: 1, pageBits: 9, expectedReads: []ReadBurstInfo{
+			{address: 0, size: 4},
+		},
+		},
+		{address: 3, size: 1, pageBits: 9, expectedReads: []ReadBurstInfo{
+			{address: 0, size: 4},
+		},
+		},
+		{address: 4, size: 1, pageBits: 9, expectedReads: []ReadBurstInfo{
+			{address: 4, size: 4},
+		},
+		},
+		{address: 0, size: 0x00100000, pageBits: 20, expectedReads: []ReadBurstInfo{
+			{address: 0, size: 0x00100000},
+		},
+		},
+		{address: 0, size: 2048, pageBits: 9, expectedReads: []ReadBurstInfo{
+			{address: 0, size: 512},
+			{address: 512, size: 512},
+			{address: 1024, size: 512},
+			{address: 1536, size: 512},
+		},
+		},
+		{address: 3, size: 523, pageBits: 9, expectedReads: []ReadBurstInfo{
+			{address: 0, size: 512},
+			{address: 512, size: 16},
+		},
+		},
+		{address: 508, size: 16, pageBits: 9, expectedReads: []ReadBurstInfo{
+			{address: 508, size: 4},
+			{address: 512, size: 12},
+		},
+		},
+		{address: 0x08007ffe, size: 16, pageBits: 15, expectedReads: []ReadBurstInfo{
+			{address: 0x08007ffc, size: 4},
+			{address: 0x08008000, size: 16},
+		},
+		},
+		{address: 0x08000000, size: 0x00020000, pageBits: 15, expectedReads: []ReadBurstInfo{
+			{address: 0x08000000, size: 0x8000},
+			{address: 0x08008000, size: 0x8000},
+			{address: 0x08010000, size: 0x8000},
+			{address: 0x08018000, size: 0x8000},
+		},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("address=%08x size=%08x pageBits=%d", tc.address, tc.size, tc.pageBits), func(t *testing.T) {
+			reads := make([]ReadBurstInfo, 0)
+			got := make([]byte, tc.size)
+			err := Read(context.Background(), got, tc.address, tc.pageBits, func(address Address, data []byte) error {
+				reads = append(reads, ReadBurstInfo{address, len(data)})
+				for k := 0; k < len(data); k++ {
+					data[k] = byte(address + Address(k))
+				}
+
+				return nil
+			})
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// Checking expected reads
+			//t.Logf("reads: %+v", reads)
+			if len(tc.expectedReads) != len(reads) {
+				t.Errorf("unexpected reads: %+v != %+v", tc.expectedReads, reads)
+			} else {
+
+				for i := 0; i < len(reads); i++ {
+					if expected, got := tc.expectedReads[i], reads[i]; expected != got {
+						t.Errorf("unexpected reads (%d): %+v != %+v", i, expected, got)
+					}
+				}
+			}
+
+			// Checking content
+			for k, v := range got {
+				expected := byte(tc.address + Address(k))
+				if expected != v {
+					t.Errorf("unexpected value: %08x != %08x", expected, v)
+					break
+				}
+			}
+
+			if t.Failed() {
+				t.Log(hexDump(got))
+			}
+		})
+	}
+}
